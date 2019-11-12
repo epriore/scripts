3088400dbc7db03d7ea951651d6c89d94ba902fe
epriore <57658126+epriore@users.noreply.github.com>
2019-11-12 18:14:18 +0100

    Create dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,14 @@
+apt update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y ocker-ce docker-ce-cli containerd.io
